e75be5eb51bc840d0ecc1f6e155eaae89710d060
Sean <contact@seanyager.com>
2020-03-30 18:31:11 -0500

    began scaffolding shop portrait page

--- src/components/ShopPortrait.vue
+++ src/components/ShopPortrait.vue
@@ -1,5 +1,25 @@
 <template>
-<section id="shopPortWrapper" class="hidden">
+
+ <section class="columns">
+      
+      <img class="sigil" src="../assets/imgs/icons/shopkeepSigilIcon.png" alt="">
+      <h3> {{  }}</h3>
+      <img class="portrait" :src="player.portrait">
+      
+      <section class="flexRow stats">
+
+          <p> {{ }} </p>
+
+      </section>
+
+      <section class="coinWrapper">
+        <img src="../assets/imgs/icons/coinIcon.png" alt="">
+        <h1 class="coinValue">{{ player.coins }}</h1>
+      </section>
+
+  </section>
+
+
 
     <!-- <img class="imgSize" src="./imgs/TRAVELERS.png" alt="">
         <section class="levelTitle">
@@ -15,15 +35,48 @@
         <section>
             <h3 id="shopTip"></h3>
         </section> -->
-</section>
+
 </template>
 
 <script>
 export default {
-  name: 'ShopPortrait',
-  props: {
-    
-  }
+    name: 'ShopPortrait',
+    data() {
+        return {
+             shopKeepers: [
+              {
+                name:"cleric", 
+                portrait:require("../assets/imgs/shopkeepers/cleric.png"), 
+                description1:"Slicing and Dicing", 
+                description2:"Bruiser class, high damage, good armor, high health.", 
+                coins:0, health:12, armor:2, attackMax:8, attackType: "physical",
+                attackTypeImage: require("../assets/imgs/icons/physicalIcon.png"), 
+                inAnimations: "zoomInLeft",
+                outAnimations: "zoomOutLeft"
+              },
+
+              {name:"mage", 
+              portrait:require("../assets/imgs/playableCharacters/mage.png"), 
+              description1:"Spellslinging", 
+              description2:"Magic attacks ignore enemy armor, highest damage, lowest health.", 
+              coins:0, health:6, armor:0, attackMax:10, attackType: "magical", 
+              attackTypeImage: require("../assets/imgs/icons/magicalIcon.png"), 
+              inAnimations: "zoomInUp",
+              outAnimations: "zoomOutDown",
+              },
+
+              {name:"varlet", 
+              portrait:require("../assets/imgs/playableCharacters/varlet.png"), 
+              description1:"Sneaky and Roguish", 
+              description2:"Avoids damage on critical hits, mid tier stats.", 
+              coins:1, health:8, armor:1, attackMax:6, attackType: "physical", 
+              attackTypeImage: require("../assets/imgs/icons/physicalIcon.png"), 
+              inAnimations: "zoomInRight",
+              outAnimations: "zoomOutRight",
+              },
+          ],
+        }
+    },
 }
 </script>
 
