c9ff42409e66ad5ce9a3474248530638364e2f4c
Sean <contact@seanyager.com>
2020-03-30 18:29:45 -0500

    wrote instructions screen helper

--- src/components/InstructionsScreen.vue
+++ src/components/InstructionsScreen.vue
@@ -1,42 +1,77 @@
 <template>
-<section id="instructions" class="hidden">
-  <p>
+<section id="instructions" class="textCenter">
+    <img src="../assets/imgs/icons/instructions.png" alt="">
+  <h1 class="uppercase">
       Welcome to the MOBEIUS DUNGEONS adventurer.
-  </p>
-  <br>
-  <p>
-      HACK & SLASH the monster, and deal your damage.
-      If it yet lives, you'll be attacked right back. 
-  </p>
-  <br>
-  <p>
-      Defeat your foe, and you'll gain their Mettle.
-      Tougher beasts boast more Mettle. Defeat the beast and you'll have a chance to buy perks.
-  </p>
-  <br>
-  <p>
+  </h1>
+
+  <h2>
+      This is a high fantasy hack and slash adventure where you attack monsters to gain coins, then spend your wealth on lavish equipment and enchantments.
+  </h2>
+
+  <h3>
+      Tougher beasts boast more coins. Each playable character is different, so try them out and see which you like best.
+  </h3>
+
+  <h3>
       Rinse. Repeat.    
-  </p>
-  <br>
+  </h3>
+
   <p>
       Do not falter.
   </p>
-  <br>
-  <h2 onclick="beginPlay()">
-      PROCEED
-  </h2>
+
+<br>
+  <h1 id="proceed" @click="$emit('understood')">
+      UNDERSTOOD.
+  </h1>
 </section>
 </template>
 
 <script>
 export default {
   name: 'InstructionsScreen',
-  props: {
-    
-  }
 }
 </script>
 
 <style scoped>
+    #instructions {
+        width:350px;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
 
+    #instructions img {
+        width:200px;
+    }
+
+    #proceed {
+        padding:20px;
+        color:black;
+        background: var(--button-background);
+    }
+    #proceed:hover {
+        background:rgb(185, 185, 185);
+        cursor:pointer;
+    }
+
+    h1 {
+        font-size:18px;
+        margin:2px;
+    }
+
+    h2 {
+        font-size:16px;
+        text-decoration: none;
+    }
+
+    h3 {
+        font-size:14px;
+    }
+
+    p {
+        font-size:10px;
+    };
 </style>
