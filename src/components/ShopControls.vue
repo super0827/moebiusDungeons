75f113708e53a2f30b4c55bd8d4ff3fa143802bc
Sean <contact@seanyager.com>
2020-03-30 18:30:49 -0500

    deleted old code, preparing to code shop phase

--- src/components/ShopControls.vue
+++ src/components/ShopControls.vue
@@ -1,20 +1,6 @@
 <template>
-<section id="shopPortWrapper" class="hidden">
-
-    <!-- <img class="imgSize" src="./imgs/TRAVELERS.png" alt="">
-        <section class="levelTitle">
-            <p id="shopName"></p>
-        </section>
-        
-        <section id="shopPortraits">
-        </section>
-        
-        <section id="shopSaying">
-        </section>
-
-        <section>
-            <h3 id="shopTip"></h3>
-        </section> -->
+<section>
+    
 </section>
 </template>
 
