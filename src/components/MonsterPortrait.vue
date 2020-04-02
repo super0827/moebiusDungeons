7a5f4458d22fc906896a37665722283cfe918111
Sean <contact@seanyager.com>
2020-04-01 20:18:14 -0500

    refactored component

--- src/components/MonsterPortrait.vue
+++ src/components/MonsterPortrait.vue
@@ -9,24 +9,59 @@
       
       <section 
       class="portraitWrapper"
+      :class="{'animated pulse' : hurt, 'animated reverseWobble' : attacking, 'animated jello' : blocked }"
+        @animationend="afterEnter"
       >
-
+        <!-- Monster Image -->
         <img 
         class="portrait" 
         :src="newMonster.portrait"
-        :class="{ 'animated pulse' : hurt }"
-        @animationend="hurt = false"
         >
 
+        <!-- Animated Damage Tips -->
         <transition appear
           type="animation"
           enter-active-class="animated jackInTheBox"
           leave-active-class="animated fadeOut"
-          @after-enter="afterEnter"
         >
           <h1 v-if="blocked" class="blocked">BLOCKED!</h1>
-          <section v-if="normalDamage" class="damagedOverlay"></section>  
         </transition>
+
+        <transition appear
+          type="animation"
+          enter-active-class="animated pulse"
+          leave-active-class="animated fadeOut"
+        >
+          <section class="damageReadout" v-if="hurt">
+              <h2 class="calculatedDamage"> {{ afterArmorDamage }} </h2>
+             
+             <section class="flexRow">
+              <h2 class="attackValue">
+                {{ attackDamage }} 
+              </h2>
+              <h3 class="versus">vs.</h3>
+              <h2 class="armorValue">
+              {{ newMonster.armor }} 
+              </h2>
+             </section>
+          </section>
+        </transition>
+          
+          <transition appear
+            type="animation"
+            enter-active-class="animated fadeIn"
+            leave-active-class="animated fadeOut"
+          >
+            <section v-if="portEffect" 
+              class="damagedOverlay"
+              :class="{ 
+                red : portEffectRed, 
+                green : portEffectGreen, 
+                purple : portEffectPurple 
+              }"
+            >
+            </section>  
+          </transition>
       </section>
       
       <section class="flexRow stats">
@@ -68,7 +103,15 @@ export default {
           //monster animation states
           blocked: false,
           hurt: false,
-          normalDamage: false,
+          attacking:false,
+          portEffect: false,
+          portEffectRed: false,
+          portEffectPurple: false,
+          portEffectGreen: false,
+
+          // Battle Data
+          attackDamage: 0,
+          afterArmorDamage: 0,
 
           //Monster data
           monsterRoster: 0,
@@ -224,9 +267,13 @@ export default {
   methods: {
     // Resets animations after they're executed
     afterEnter: function (){
-    console.log('After Enter');
     this.blocked = false;
     this.hurt = false;
+    this.attacking = false;
+    this.portEffect = false;
+    this.portEffectRed = false;
+    this.portEffectGreen = false;
+    this.portEffectPurple = false;
     }
   },
   created() {
@@ -237,17 +284,38 @@ export default {
 
   },
    mounted() { 
-    // let self = this;
 
-    EventBus.$on('monster-recoil', () => { this.hurt = true; });
+    EventBus.$on('monster-attacking', () => {
+      this.attacking = true;
+    });
+
+    EventBus.$on('monster-recoil', () => {
+      this.hurt = true; 
+      this.portEffect = true;
+      this.portEffectRed = true;
+    });
+    //listens for physical damage
+    EventBus.$on('monster-physical-damage', ($event) => {
+      this.attackDamage = $event;
+      this.afterArmorDamage = this.attackDamage - this.newMonster.armor;
+    });
     //listen for player dealing 0 or less damage
-    EventBus.$on("monster-blocked", () => { this.blocked = true; });
+    EventBus.$on("monster-blocked", () => { 
+      this.blocked = true; 
+      this.portEffect = true;
+      this.portEffectPurple = true;
+    });
     //listens fot player dealing 1 or more damage.
-    EventBus.$on("monster-takes-damage", () => { this.newMonster.health--; });
+    EventBus.$on("monster-takes-damage", () => { 
+      this.newMonster.health--;
+    });
     //listens for monster dying
     EventBus.$on("is-monster-dead", () => { 
       if(this.newMonster.health <= 0){
-        // self.$emit('monster-is-dead');
+        console.log('monster is dead!');
+        EventBus.$emit('monster-retaliate');
+      } else {
+          EventBus.$emit('monster-retaliate');
       }
     });
   },
@@ -261,6 +329,77 @@ export default {
   height:233px;
   grid-template-columns: 100%;
   grid-template-rows: 33% 34% 33%;
+}
+
+.attackValue{
+  color:rgb(255, 166, 0);
+  text-shadow: black 2px 2px 2px;
+  font-size:40px;
+  margin:5px;
+  padding:5px;
+  text-decoration: none;
+}
+
+.armorValue {
+  color:rgb(0, 179, 255);
+  text-shadow: black 2px 2px 2px;
+  font-size:40px;
+  margin:5px;
+  padding:5px;
+  text-decoration: none;
+}
+
+.calculatedDamage {
+  color:red; 
+  text-shadow: black 2px 2px 2px;
+  font-size:65px;
+  line-height:65px;
+  margin:0;
+  padding:0;
+  text-decoration: none;
+}
+
+.versus {
+    line-height:40px;
+    font-size:20px;
+    margin:0;
+    padding:0;
+    color:rgb(192, 192, 192);
+}
+
+.red {
+    background: linear-gradient(0deg, rgb(187, 16, 58) 0%, rgba(255,192,0,0) 100%);
+}
+
+.purple {
+    background: linear-gradient(0deg, rgb(187, 16, 187) 0%, rgba(255,192,0,0) 100%);
+
+}
+
+.green {
+    background: linear-gradient(0deg, rgb(16, 187, 87) 0%, rgba(255,192,0,0) 100%);
+
+}
+
+.damagedOverlay {
+  grid-row:3/4;
+  grid-column:1/2;
+}
+
+.damageReadout {
+    grid-row: 2/3;
+    grid-column: 1/2;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width:200px;
+    text-shadow: black 2px 2px 2px;
+    text-align:center;
+    margin:0 auto;
+    text-decoration:none;
+    font-family:var(--paragraphs-type);
+
 }
 
 .blocked {
