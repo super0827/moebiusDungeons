623a3d3c728b8d419f3cec2c286f3aa6d16a107b
Sean Yager <contact@seanyager.com>
2020-04-05 18:41:48 -0500

    changed .flexRow to align-items to top

--- src/components/ShopControls.vue
+++ src/components/ShopControls.vue
@@ -5,10 +5,55 @@
     leave-to-class="amimated zoomOutUp"
 >
 <section class="columns battleOptions">
+  
+  <!-- Shop Title Description -->
+  <h2 class="textCenter">{{ shopkeepData.shopTitle }}</h2>
 
-    <h2 :class="{'striked' : !tierOne}">{{ shopKeep.items[0].name }}</h2>
-    <h2 :class="{'striked' : !tierTwo}">{{ shopKeep.items[1].name }}</h2>
-    <h2 :class="{'striked' : !tierThree}">{{ shopKeep.items[2].name }}</h2>
+  <hr>
+
+    <section class="buySlot">
+      <section class="information">
+        <article class="cost">
+          <img src="../assets/imgs/icons/coinIcon.png">
+          <h1>{{ shopkeepData.items[0].cost }}</h1>
+        </article>
+      
+        <article class="description">
+          <h2 :class="{'striked' : !tierOne}">{{ shopkeepData.items[0].name }}</h2>
+          <p> {{ shopkeepData.items[0].description }} </p>
+        </article>
+      </section>
+    </section>
+
+    <section class="buySlot">
+      <section class="information">
+        <article class="cost">
+          <img src="../assets/imgs/icons/coinIcon.png">
+          <h1>{{ shopkeepData.items[1].cost }}</h1>
+        </article>
+      
+        <article class="description">
+          <h2 :class="{'striked' : !tierOne}">{{ shopkeepData.items[1].name }}</h2>
+          <p> {{ shopkeepData.items[1].description }} </p>
+        </article>
+      </section>
+    </section>
+
+    <section class="buySlot">
+      <section class="information">
+        <article class="cost">
+          <img src="../assets/imgs/icons/coinIcon.png">
+          <h1>{{ shopkeepData.items[2].cost }}</h1>
+        </article>
+      
+        <article class="description">
+          <h2 :class="{'striked' : !tierOne}">{{ shopkeepData.items[2].name }}</h2>
+          <p> {{ shopkeepData.items[2].description }} </p>
+        </article>
+      </section>
+    </section>
+
+<hr>
 
 </section>
 </transition>
@@ -32,15 +77,97 @@ export default {
 </script>
 
 <style scoped>
+.information {
+  display:flex;
+  justify-content:space-around;
+  align-items: center;
+}
+
+.description {
+  display:inline-block;
+}
+
+.itemWrapper {
+  margin-top:10px;
+}
+
+.textCenter {
+  margin:10px 10px 20px 10px;
+}
+
 h2 {
-  padding:10px;
-  font-size: 17px;
+  font-size: 15px;
   text-decoration: none;
-  background: rgb(207, 207, 207);
+  text-align:center;
+  margin:0;
+  padding:0;
+}
+
+article h2 {
+  text-align: center;
+  font-size:15px;
+}
+
+p {
+  display:block;
+  text-align:center;
+  font-size:12px;
+  padding:0 0 0px 0;
+  margin:0;
 }
 
 .battleOptions {
   min-width:200px;
-  width:200px
+  width:200px;
+}
+
+.buySlot {
+  position:relative;
+  padding:10px;
+  background: rgba(207, 207, 207, 1);
+  margin:0 0 10px 0;
+}
+
+.buySlot:hover {
+  background: rgb(224, 224, 224);
+  cursor:pointer;
+}
+
+.buySlot:first-of-type {
+  margin:0px 0px 10px 0px;
+}
+
+.buySlot:last-of-type {
+  margin:10px 0 0 0;
+}
+
+.cost {
+  position:relative;
+}
+
+.cost > img {
+  width:55px;
+  max-width:55px;
+  margin:0 10px 0 0;
+}
+
+.cost h1 {
+  position:absolute;
+  margin:0;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  line-height: 55px;
+  top:0px;
+  width:55px;
+  left: 0;
+}
+
+.flexRow {
+  height:50px;
+}
+
+h1 {
+  color:black;
+  text-shadow: rgb(255, 153, 0) 2px 2px 2px;
 }
 </style>
--- src/components/ShopPortrait.vue
+++ src/components/ShopPortrait.vue
@@ -17,7 +17,6 @@
 </template>
 
 <script>
-import { EventBus } from '../js/event-bus';
 
 export default {
     name: 'ShopPortrait',
@@ -29,38 +28,54 @@ export default {
                 name:"cleric", 
                 portrait:require("../assets/imgs/shopkeepers/cleric.png"),
                 items: [
-                    { name: 'minor heal', cost: 1, description: 'heals player for 6 hp', value:6, },
-                    { name: 'blessing', cost: 2, description: 'increases players armor by 1', value: 3 },
-                    { name: 'miracle', cost: 3, description: 'increases players max attack by 3', value: 3 },
+                    { name: 'minor heal', cost: 1, description: 'heals player for 6 hp' },
+                    { name: 'blessing', cost: 2, description: 'increases players armor by 1' },
+                    { name: 'miracle', cost: 3, description: 'increases players max attack by 3' },
                 ],
                 saying:"Come in, are you hurt?",
-                inAnimations: "zoomInRight",
-                outAnimations: "zoomOutRight"
+                shopTitle: "I can heal you, or perhaps you need the favor of the old gods?"
               },
 
               {
                 name:"graverobber", 
-              portrait:require("../assets/imgs/shopkeepers/graverobber.png"), 
-              inAnimations: "zoomInRight",
-              outAnimations: "zoomOutRight",
+              portrait:require("../assets/imgs/shopkeepers/graverobber.png"),
+              items: [
+                { name: 'nacre charm', cost: 1, description: 'ATK +1 | ARM +1 | HP +1'},
+                { name: 'detriment bangle', cost: 2, description: "Halves your HP | +3 ARM"},
+                { name: 'Demon Ring', cost: 3, description: 'ATK Type becomes Pyhsical | x2 ATK '},
+              ],
+              saying:"Trust me, nothing I sell is cursed.",
+              shopTitle: "Just buy somethin' quick, I don't wanna be seen fencing to the Kingloyal."
               },
 
-              {name:"varlet", 
-              portrait:require("../assets/imgs/playableCharacters/varlet.png"), 
-              description1:"Sneaky and Roguish", 
-              description2:"Avoids damage on critical hits, mid tier stats.", 
-              coins:1, health:8, armor:1, attackMax:6, attackType: "physical", 
-              attackTypeImage: require("../assets/imgs/icons/physicalIcon.png"), 
-              inAnimations: "zoomInRight",
-              outAnimations: "zoomOutRight",
+              {name:"merchant", 
+              portrait:require("../assets/imgs/shopkeepers/merchant.png"),
+              items: [
+                { name: 'rations', cost: 1, description: '+5 HP' },
+                { name: 'armor kit', cost: 2, description: '+1 ARM' },
+                { name: 'whetstone', cost: 2, description: '+2 ATK' },
+              ],
+              saying:"Friend or foe, what are ya' buyin'?",
+              shopTitle: "It's not much, but it's what I've got. All priced to move."
+              },
+
+              {name:"witch", 
+              portrait:require("../assets/imgs/shopkeepers/witch.png"),
+              items: [
+                { name: 'red potion', cost: 1, description: '+8 HP' },
+                { name: 'banded charm', cost: 3, description: '+2 ATK | +1 ARM' },
+                { name: 'blood ritual', cost: 5, description: 'Thirds your HP | + lost HP to your ATK' },
+              ],
+              saying:"Everything you see, all hand enchanted.",
+              shopTitle: "Some of my inventory tends to be virulent. Browse at your own risk."
               },
           ],
         }
     },
     created() {
-        // let randomNumber = Math.floor(Math.random() * Math.floor(4) + 1);
-        this.currentShopkeep = this.shopKeepers[0];
-        EventBus.$emit('send-shopkeep', this.currentShopkeep);
+        let randomNumber = Math.floor(Math.random() * Math.floor(3) + 1);
+        this.currentShopkeep = this.shopKeepers[randomNumber];
+        this.$emit('send-shopkeep', this.currentShopkeep);
     }
 }
 </script>
