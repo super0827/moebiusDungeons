c145687a19b569bf6422ea03166e9ad957fbc888
Sean <contact@seanyager.com>
2020-04-24 15:39:49 -0500

    added SoundPlugin to main

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false;
 
+import SoundPlugin from '@/plugins/SoundPlugin';
+Vue.use(SoundPlugin);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
