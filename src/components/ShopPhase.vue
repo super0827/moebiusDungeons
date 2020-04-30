f77938c60cbf10f93fea10471868ca1715a41118
Sean <contact@seanyager.com>
2020-04-29 23:57:21 -0500

    Added helper to ShopPhase

--- src/components/ShopPhase.vue
+++ src/components/ShopPhase.vue
@@ -1,11 +1,16 @@
 <template>
-<section key="shopPhaseWrapper">
+<section class="shopPhaseWrapper" key="shopPhaseWrapper">
     <section class="flexColumn animated"
         :class="{'zoomInDown' : storeState.isEntering, 'zoomOutUp' : !storeState.isEntering}">
     
         <img key="shopSigil" class="iconImageSize" src="../assets/imgs/icons/shopkeepSigilIcon.png" >
         <h1 class="textCenter phaseName">SHOP</h1> 
     </section>
+
+    <!-- Battle Helper Button -->
+    <transition name="fade" mode="out-in">
+        <h1 @click="helper = !helper" @mouseenter="$sound.play('chit')" id="shopHelp">SHOP HELP</h1>
+    </transition>
 
     <section class="flexRow">
         <!-- Player Portrait -->
@@ -26,16 +31,21 @@
         :class="{'zoomInRight' : storeState.isEntering, 'zoomOutRight' : !storeState.isEntering }"
         key="shopPortrait"/>
     </section>
+
+    <!--  Battle Helpers -->
+    <transition name="fade" mode="out-in">
+        <shop-help key="battleHelper" @close="helper = false" v-if="helper==true"/>
+    </transition>
 </section>
 </template>
 
 <script>
 import { store } from "../store";
-import { Howl } from "howler";
 
 import ShopControls from './ShopControls.vue';
 import PlayerPortrait from "./PlayerPortrait.vue";
 import ShopPortrait from "./ShopPortrait.vue";
+import ShopHelp from "./ShopHelp.vue";
 
 
 
@@ -45,27 +55,56 @@ export default {
         PlayerPortrait,
         ShopControls,
         ShopPortrait,
+        ShopHelp,
     },
     data() {
         return {
             storeState: store.state,
-            shopMusic: new Howl ({
-                src: [require('../assets/audio/spiritualMomentPP.mp3')],
-                volume:0.5,
-                loop: true,
-            }),
+            helper:false,
+            randomBkg: Number,
+        }
+    },
+    methods: {
+        randomRoll(rollMax){
+            return Math.floor(Math.random() * Math.floor(rollMax) + 1);
         }
     },
     created() {
-        this.shopMusic.play();
-        this.shopMusic.fade(0, .3, 1500);
+        this.randomBkg = this.randomRoll(3);
+        this.$sound.play(`shopMusic${this.randomBkg}`, {fade: 1200, volume: .2});
     },
     beforeDestroy() {
-        this.shopMusic.fade(.3, 0, 1500);
+        this.$sound.pause(`shopMusic${this.randomBkg}`, {fade: 1200, volume: 0});
     },
+    watch: {
+        helper: function() {
+            if(this.helper === true){
+                this.$sound.pause(`dungeonMusic${this.randomBkg}`, {fade: 1200, volume:.05})
+            } else {
+                this.$sound.pause(`dungeonMusic${this.randomBkg}`, {fade: 1200, volume:.2})
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
-
+.shopPhaseWrapper {
+        position:relative;
+    }
+#shopHelp {
+    font-size: 20px;
+    position: fixed;
+    right: 10px;
+    top: 10px;
+    margin: 0;
+    padding: 5px;
+    background: rgb(218, 218, 218);
+    cursor:pointer;
+}
+shop-help {
+    position:absolute;
+    z-index:99999;
+    transition: all .4s ease-in-out;
+}
 </style>
