66441543a91844a5be9b95da38529818e6fc83a3
Sean <contact@seanyager.com>
2020-05-12 12:48:11 -0500

    adding a global mixin to handle animation transitions

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from './store/store'
 
 Vue.config.productionTip = false;
 
 import SoundPlugin from '@/plugins/SoundPlugin';
+import { mapState } from 'vuex';
 Vue.use(SoundPlugin);
+
+
+//global mixins
+Vue.mixin({
+  beforeCreate: function () {
+    store.commit('enterAnimation', true);
+  },
+  beforeDestroy: function () {
+    store.commit('enterAnimation', false);
+  },
+  computed: mapState([
+    'isEntering',
+    'phase'
+  ]),
+})
 
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
