23a6ec59871812e3a330e163efb6a8b5f6c4d24f
Sean <contact@seanyager.com>
2020-05-18 19:35:24 -0500

    new component to replace duplicate components

---
+++ src/components/CharacterStats.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <section class="stats statColumn">
+
+        <section class="flexRowStats"
+        :class="{ 'reverseSides': who === 'monster' }"
+        >
+          <img src="../assets/imgs/icons/healthIcon.png">
+          <p>
+          <animated-number 
+          :value="health"
+          :duration="1000"
+          :formatValue="wholeNumber"
+          />
+          </p>
+        </section>
+        
+        <section class="flexRowStats"
+        :class="{ 'reverseSides': who==='monster' }"
+        >
+          <img src="../assets/imgs/icons/armorIcon.png">
+          <p>
+            <animated-number 
+          :value="armor"
+          :duration="1000"
+          :formatValue="wholeNumber"
+          />
+          </p>
+        </section>
+        
+        <section class="flexRowStats"
+        :class="{ 'reverseSides': who==='monster' }"
+        >
+          <img :src="attackImage">
+          <p>
+          <animated-number 
+          :value="attack"
+          :duration="1000"
+          :formatValue="wholeNumber"
+          />
+          </p>
+        </section>
+
+      </section>  
+    </div>
+</template>
+
+<script>
+import AnimatedNumber from "animated-number-vue";
+
+export default {
+    name: 'CharacterStats',
+    components: {
+        AnimatedNumber,
+    },
+    props: [
+      'who',
+      'health',
+      'armor',
+      'attack',
+      'attackImage',
+    ],
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style scoped>
+.flexRowStats {
+    display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+.reverseSides {
+  flex-direction:row-reverse;
+}
+
+p { 
+  font-size:25px;
+}
+</style>
