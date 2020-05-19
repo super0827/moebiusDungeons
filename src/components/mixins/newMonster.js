9ad036ed285930afd3935deb34971a5f4610b520
Sean <contact@seanyager.com>
2020-05-18 19:37:48 -0500

    clarified mutation names

--- src/components/mixins/newMonster.js
+++ src/components/mixins/newMonster.js
@@ -4,9 +4,9 @@ export default {
     //scene change animations, 
     //bkg music handling, 
     created: function () {
-        store.commit('mutate', {property: 'isEntering', with: true});
+        store.commit('monsterData/newMonster', {property: 'isEntering', with: true});
     },
     beforeDestroy: function () {
-        store.commit('mutate', {property: 'isEntering', with: false});
+        store.commit('monsterData/mutate', {property: 'isEntering', with: false});
     },
 };
