be6d5ee3bf2bbf128c3503dc7b7dfd798a7de1c1
Sean <contact@seanyager.com>
2020-05-22 16:47:08 -0500

    +combat logic

--- src/store/modules/playerData.js
+++ src/store/modules/playerData.js
@@ -1,3 +1,5 @@
+import { mapActions } from "vuex";
+
 const state = () => ({
     info:  {
       name:"swordsman",
@@ -10,9 +12,9 @@ const state = () => ({
     },
     permenantTraits: [],
     tempTraits: [],
-    thisDamage: {type: Number},
-    thisAdjDamage: {type: Number},
+    thisDamage: 0,
     log: [],
+    logNum: 0,
     animations: {
       blocking: false,
       hurt: false,
@@ -26,30 +28,118 @@ const state = () => ({
 })
 
 const mutations = {
-    mutate(state, payload) {
-      state[payload.property] = payload.with;
-    },
-    addHealth(state) {
-      state.info.health += 10;
-    },
-    loseHealth(state) {
-      state.info.health -= 10;
-    },
-    addArmor(state) {
-      state.info.armor += 1;
-    },
-    loseArmor(state) {
-      state.info.armor -= 1;
+  mutate(state, payload) {
+    state[payload.property] = payload.with;
+  },
+  toggle(state, payload) {
+    state.animations[payload.property] = !state.animations[payload.property];
+  },
+  incrementLog(state) {
+    state.logNum++
+  },
+  take_damage(state, payload) {
+    state.info.health -= payload.damage;
+  },
+  addToLog(state, payload){
+    state.log.push(payload)
+  },
+  addHealth(state) {
+    state.info.health += 10;
+  },
+  loseHealth(state) {
+    state.info.health -= 10;
+  },
+  addArmor(state) {
+    state.info.armor += 1;
+  },
+  loseArmor(state) {
+    state.info.armor -= 1;
+  }
+}
+
+const getters = {
+  thisAdjDamage: (state, getters, rootState) => {
+    let num;
+    if( state.info.attackType === 'physical') {
+      num = state.thisDamage - rootState.monsterData.info.armor;
     }
+    else if ( state.info.attackType === 'magical') {
+      num = state.thisDamage;
+    }
+    if(num <= 0) return 0
+    else return num
+  },
+  playerLog: (state) => {
+    let maxLog = state.log
+    if(maxLog.length > 4) {
+      maxLog.shift();
+    }
+    return maxLog
+  }
 }
 
 const actions = {
-  
+  ROLL_DAMAGE({commit, state, getters}) {
+    return new Promise((resolve) => {
+        // commit('gameData/toggle', {property:'combatLocked'}, {root: true});
+        const randomRoll = Math.floor(Math.random() * (state.info.attackMax) + 1)
+        console.log(`monster attack: ${randomRoll}`);
+        commit('mutate', {property:'thisDamage', with:randomRoll})
+        setTimeout(() => {
+          if(state.thisDamage == randomRoll) resolve('damage')
+        }, 200)
+    })
+    
+  },
+  TRADE_BLOWS({commit, state, dispatch, getters}){
+    //ROLL FOR DAMAGE
+    dispatch('ROLL_DAMAGE')
+    //ANIMATE ATTACK AND DEAL DAMAGE
+    .then(() => { 
+      dispatch('DEAL_DAMAGE')
+    })
+    //ADD DAMAGE AMMOUNT TO LOG
+    .then(() => {
+      dispatch('LOG_UPDATE', `TRADE BLOWS DEALT ${getters.thisAdjDamage} DAMAGE`)
+    })
+    //Monster Retaliates
+    .then(() => {
+      dispatch('monsterData/TRADE_BLOWS', null, {root:true})
+    })
+  },
+  LOG_UPDATE({commit, getters, state}, payload) {
+    commit('addToLog', {id:state.logNum, message:payload});
+    commit('incrementLog')
+  },
+  DEAL_DAMAGE({commit, getters}) {
+    return new Promise((resolve) => {
+      commit('toggle', {property:'attacking'});
+      commit('monsterData/take_damage', {damage: getters.thisAdjDamage}, {root:true})
+
+    })
+  },
+  RUN_SPECIAL(context){
+
+  },
+  TURN_TAIL(context){
+    //calculate success chance
+      //roll die for player and monster
+      //add their health to the roll
+      //display in the player log
+    //if player's roll is higher then they get away
+    //run escape
+  },
+  ESCAPE(context){
+    //animate escape
+    //change phase
+  }
 }
 
 export default {
     namespaced: true,
     state,
     mutations,
+    getters,
+    actions,
 }
         
