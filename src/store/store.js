8dfc1c5b81775ba6052e6ac0a3cae294ae1a0f7e
Sean <contact@seanyager.com>
2020-05-23 16:09:15 -0500

    added vuex logger to the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import createLogger from 'vuex/dist/logger'
+
 import gameData from './modules/gameData'
 import musicData from './modules/musicData'
 import playerData from './modules/playerData'
@@ -10,7 +12,39 @@ import leaderboardData from './modules/leaderboardData'
 
 Vue.use(Vuex);
 
+const logger = createLogger({
+  collapsed: false, // auto-expand logged mutations
+  filter (mutation, stateBefore, stateAfter) {
+    // returns `true` if a mutation should be logged
+    // `mutation` is a `{ type, payload }`
+    return mutation.type !== "aBlacklistedMutation"
+  },
+  actionFilter (action, state) {
+    // same as `filter` but for actions
+    // `action` is a `{ type, payload }`
+    return action.type !== "aBlacklistedAction"
+  },
+  transformer (state) {
+    // transform the state before logging it.
+    // for example return only a specific sub-tree
+    return state.subTree
+  },
+  mutationTransformer (mutation) {
+    // mutations are logged in the format of `{ type, payload }`
+    // we can format it any way we want.
+    return mutation.type
+  },
+  actionTransformer (action) {
+    // Same as mutationTransformer but for actions
+    return action.type
+  },
+  logActions: true, // Log Actions
+  logMutations: true, // Log mutations
+  logger: console, // implementation of the `console` API, default `console`
+})
+
 export const store = new Vuex.Store({
+  // plugins: process.env.NODE_ENV !== 'production' ? [logger] : [],
   modules: {
     gameData,
     musicData,
