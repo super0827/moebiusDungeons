423e8aab36b9dd5ab800bde5c74b5ab07220d3fd
Sean <contact@seanyager.com>
2020-05-29 19:06:56 -0500

    reworked source of player data to be computed from multiple sources

--- src/store/modules/gameData.js
+++ src/store/modules/gameData.js
@@ -1,7 +1,7 @@
 import { shuffle } from 'lodash.shuffle'
 
 const state = () => ({
-    phase: "StartScreen",
+    phase: "ShopPhase",
     isEntering: true,
     helper: false,
     duration: 1000,
@@ -26,9 +26,9 @@ const actions = {
 
 const getters = {
     // HEALTH BAR CALCULATOR
-    healthBar: (state, getters, rootState) => {
-        const monster = rootState.monsterData.info.health
-        const player = rootState.playerData.info.health
+    healthBar: (state, getters, rootState, rootGetters) => {
+        const monster = rootGetters['monsterData/calcHealth']
+        const player = rootGetters['playerData/calcHealth']
         const hpPool =  monster + player
         if (monster <= 0) return 100
         else if (player <= 0) return 0
@@ -36,9 +36,9 @@ const getters = {
     },
 
     //ARMOR BAR CALCULATOR
-    armorBar: (state, getters, rootState) => {
-        const monster = rootState.monsterData.info.armor
-        const player = rootState.playerData.info.armor
+    armorBar: (state, getters, rootState, rootGetters) => {
+        const monster = rootGetters['monsterData/calcArmor']
+        const player = rootGetters['playerData/calcArmor']
         const armPool =  monster + player
         if (monster <= 0) return 100
         else if (player <= 0) return 0
@@ -46,9 +46,9 @@ const getters = {
     },
 
     //ATTACK BAR CALCULATOR
-    attackBar: (state, getters, rootState) => {
-        const monster = rootState.monsterData.info.attackMax
-        const player = rootState.playerData.info.attackMax
+    attackBar: (state, getters, rootState, rootGetters) => {
+        const monster = rootGetters['monsterData/calcAttackMax']
+        const player = rootGetters['playerData/calcAttackMax']
         const armPool =  monster + player
         if (monster <= 0) return 100
         else if (player <= 0) return 0
