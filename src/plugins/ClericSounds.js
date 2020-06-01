2e224438f8c1c0103ccb778f26390f73fdf48ecd
Sean <contact@seanyager.com>
2020-06-01 10:27:26 -0500

    +new sound files

---
+++ src/plugins/ClericSounds.js
@@ -0,0 +1,187 @@
+import {Howl, Howler} from 'howler'
+
+const hello = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/hello.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const hello2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/hello2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const goodbye = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/goodbye.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const goodbye2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/goodbye2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const beCareful = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/beCareful.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const beCareful2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/beCarfeul2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const byAmara = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/byAmara.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const byAmara2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/byAmara2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const feelingBetter = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/feelingBetter.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const feelingBetter2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/feelingBetter2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const moreCoins = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/moreCoins.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const moreCoins2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/moreCoins2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const seeYouSoon = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/seeYouSoon.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const seeYouSoon2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/seeYouSoon2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const staySafe = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/staySafe.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const staySafe2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/staySafe2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const thanks = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/thanks.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const thanks2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/thanks2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const thankYou = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/thankYou.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const thankYou2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/thankYou2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const notEnough = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/notEnough.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const notEnough2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/notEnough2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const welcome = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/welcome.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const welcome2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/welcome2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const youreBack = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/youreBack.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const youreBack2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/cleric/youreBack2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+export default {
+    hello,
+    hello2,
+    goodbye,
+    goodbye2,
+    beCareful,
+    beCareful2,
+    byAmara,
+    byAmara2,
+    feelingBetter,
+    feelingBetter2,
+    moreCoins,
+    moreCoins2,
+    seeYouSoon,
+    seeYouSoon2,
+    staySafe,
+    staySafe2,
+    thanks,
+    thanks2,
+    thankYou,
+    thankYou2,
+    notEnough,
+    notEnough2,
+    welcome,
+    welcome2,
+    youreBack,
+    youreBack2
+};
+
---
+++ src/plugins/WitchSounds.js
@@ -0,0 +1,166 @@
+import {Howl, Howler} from 'howler'
+
+const hello = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/hello.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const hello2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/hello2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const hello3 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/hello3.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const oh = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/oh.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const ohHello = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/ohHello.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const goodbye = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/goodbye.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const goodbye2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/goodbye2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const carefulWithThat = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/carefulWithThat.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const carefulWithThat2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/carefulWithThat2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const ahThanks = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/ahThanks.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const ahThanks2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/ahThanks2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const interestingChoice = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/interestingChoice.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const interestingChoice2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/interestingChoice2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const notEnough = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/notEnough.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const notEnough2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/notEnough2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const itsAllYours = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/itsAllYours.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const itsAllYours2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/itsAllYours2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const thankYou = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/thanksYou.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const thankYou2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/thankYou2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const welcomeBack = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/welcomeBack.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const welcomeBack2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/welcomeBack2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const seeYouSoon = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/seeYouSoon.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+const seeYouSoon2 = new Howl({
+    src: [require('@/assets/audio/shopAudio/witch/seeYouSoon2.mp3')],
+    loop: false,
+    volume: 1,
+});
+
+export default {
+    hello,
+    hello2,
+    hello3,
+    oh,
+    ohHello,
+    goodbye,
+    goodbye2,
+    carefulWithThat,
+    carefulWithThat2,
+    ahThanks,
+    ahThanks2,
+    interestingChoice,
+    interestingChoice2,
+    notEnough,
+    notEnough2,
+    itsAllYours,
+    itsAllYours2,
+    thankYou,
+    thankYou2,
+    welcomeBack,
+    welcomeBack2,
+    seeYouSoon,
+    seeYouSoon2,
+};
+
