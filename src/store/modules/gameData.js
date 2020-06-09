01a6bf9bc85b76eb2bdb1819f209543bf027978b
Sean <contact@seanyager.com>
2020-06-09 12:03:42 -0500

    new calculations made for stat bars

--- src/store/modules/gameData.js
+++ src/store/modules/gameData.js
@@ -1,7 +1,7 @@
 import { shuffle } from 'lodash.shuffle'
 
 const state = () => ({
-    phase: "DungeonPhase",
+    phase: "CharacterSelect",
     isEntering: true,
     helper: false,
     duration: 1000,
@@ -27,8 +27,8 @@ const actions = {
 const getters = {
     // HEALTH BAR CALCULATOR
     healthBar: (state, getters, rootState, rootGetters) => {
-        const monster = rootGetters['monsterData/calcHealth']
         const player = rootGetters['playerData/calcHealth']
+        const monster = rootGetters['monsterData/calcHealth']
         const hpPool =  monster + player
         if (monster <= 0) return 100
         else if (player <= 0) return 0
@@ -36,23 +36,29 @@ const getters = {
     },
 
     //ARMOR BAR CALCULATOR
-    armorBar: (state, getters, rootState, rootGetters) => {
+    playerAttackVsArmor: (state, getters, rootState, rootGetters) => {
+        const player = rootGetters['playerData/calcAttackMax']
         const monster = rootGetters['monsterData/calcArmor']
-        const player = rootGetters['playerData/calcArmor']
-        const armPool =  monster + player
-        if (monster <= 0) return 100
-        else if (player <= 0) return 0
-        else return (player / armPool) * 100
+
+        const reducedDamage = player - monster;
+
+        if (reducedDamage <= 0) return 5
+        else if (monster === 0) return 100
+        else if (monster > player) return 5
+        else return (reducedDamage * 10)
     },
 
     //ATTACK BAR CALCULATOR
-    attackBar: (state, getters, rootState, rootGetters) => {
+    playerArmorVsAttack: (state, getters, rootState, rootGetters) => {
+        const player = rootGetters['playerData/calcArmor']
         const monster = rootGetters['monsterData/calcAttackMax']
-        const player = rootGetters['playerData/calcAttackMax']
-        const armPool =  monster + player
+        const armVsMonAtk =  monster + player
+        const monsterType = rootState['monsterData'].info.attackType
+        console.log(`Monster Type = ${monsterType}`)
         if (monster <= 0) return 100
-        else if (player <= 0) return 0
-        else return (player / armPool) * 100
+        else if (player <= 1) return 5
+        else if (rootState['monsterData'].info.attackType === 'magical') return 5;
+        else return (player / monster) * 100
     },
 }
 
