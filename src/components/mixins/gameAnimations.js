c1d5e2e1c9fdaabd73f0196dc40f0fbbaab370f9
Sean <contact@seanyager.com>
2020-06-10 01:22:55 -0500

    removed unused code

--- src/components/mixins/gameAnimations.js
+++ src/components/mixins/gameAnimations.js
@@ -8,18 +8,3 @@ export default {
         store.commit('gameData/mutate', {property: 'isEntering', with: false});
     },
 };
-
-
-
-// beforeDestroy: function(){
-//     if(store.state.gameData.phase == 'DungeonPhase') {
-//         if(this.state.monsterRoster > this.characters.monsterCharacters.length) {
-//             this.state.monsterRoster = this.characters.monsterCharacters.length;
-//             this.state.finalBoss = true; 
-// 		}
-// 		else {
-//             this.state.monster = this.characters.monsterCharacters[this.state.monsterRoster];
-// 		}
-// 			console.log(`new monster is ${this.state.monsters.name}`);
-//     	}
-// },
