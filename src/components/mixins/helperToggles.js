b5f8b486407329f521dd56badb0e66a7a2369ef1
Sean <contact@seanyager.com>
2020-06-11 20:06:19 -0500

    deleted most of helper code here, plugin is a simple toggle

--- src/components/mixins/helperToggles.js
+++ src/components/mixins/helperToggles.js
@@ -1,24 +1,7 @@
 import { store } from '../../store/store'
-import { mapGetters } from 'vuex'
 import bkg from '@/plugins/backgroundMusic.js'
 
 export default {
-    watch: {
-        //helpers toggling, audio ducking
-        helper: function(value){
-            if (value == true) {
-                bkg[this.bkgMusic].fade(1,.2,1000); 
-            }
-            else {
-                bkg[this.bkgMusic].fade(.2,1,1000); 
-            }
-        }
-    },
-    computed: {
-        ...mapGetters( 'musicData', {
-            bkgMusic: 'musicPick'
-        })
-    },
     methods: {
         toggleHelp() {
             store.commit('gameData/toggleHelp');
