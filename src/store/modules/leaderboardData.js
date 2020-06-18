c5f2c31d4023e3be2ed214d0241708f6530cca1b
Sean Yager <contact@seanyager.com>
2020-06-17 23:39:26 -0500

    added leaderboard data

--- src/store/modules/leaderboardData.js
+++ src/store/modules/leaderboardData.js
@@ -1,11 +1,21 @@
 const state = () => ({
-  stats: {type: Object},
+  highestHealth:0,
+  highestArmor:0,
+  highestAttack:0,
+  monstersKilled:0,
+  monstersFought: [],
+  coinsSpent:0,
+  totalDamageDealt:0,
+  totalDamageTaken:0,
+  damageBlocked:0,
+  itemsBought:[],
+  totalCoins:0,
 })
 
 const mutations = {
-    mutate(state, payload) {
-        state[payload.property] = payload.with;
-      }
+  mutate(state, payload) {
+    state[payload.property] = payload.with;
+  }
 }
 
 export default {
