f22f09184f03d2acb65352e3afa9c4c925c1dc0d
Sean Yager <contact@seanyager.com>
2020-09-09 18:19:44 -0500

    integrating retirement into the game flow

--- src/components/ShopSelect.vue
+++ src/components/ShopSelect.vue
@@ -25,47 +25,101 @@
         </section>
     </section>
 
+    <h3 class="marginTop">Or pick an option below</h3>
+
+    <section class="flexRow">
+        <h2 @click="addMettle()" class="button green">
+            Rest + 1 <img :src="mettleImg"/>
+        </h2>
+    </section>
+
+    <section class="flexRow">
+        <h2 @click="toggleRetire()" class="button red">
+            Retire From Your Service
+        </h2>
+    </section>
+
+<h1 id="about" @mouseenter="UiSound.chit.play()" @click="toggleHelp()"> Shop Select Help </h1>
+
+    <!--  Battle Helpers -->
+    <transition name="fade" mode="out-in">
+        <shop-select-help key="battleHelper" @close="toggleHelp()" v-if="helper"/>
+    </transition>
+   
+    <!--  Retire Confirmation -->
+    <transition name="fade" mode="out-in">
+        <retire key="retireConfirm" @close="toggleRetire()" @retire="retireGame()" v-if="retire"/>
+    </transition>
+
+
 </section>
 </template>
 
 <script>
 import InstructionsScreen from './InstructionsScreen';
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 import shuffle from 'lodash.shuffle'
 
+import ShopSelectHelp from "./ShopSelectHelp.vue";
+import Retire from "./Retire.vue";
 import UiSound from '@/plugins/UiSounds.js'
 
 import helperToggles from './mixins/helperToggles';
 import gameAnimations from './mixins/gameAnimations';
 import gameMusic from './mixins/gameMusic';
 
+import firebase from "firebase";
+
 export default {
   name: 'ShopSelect',
   mixins: [helperToggles, gameAnimations, gameMusic],
   components: {
       InstructionsScreen,
+      ShopSelectHelp,
+      Retire
   },
   data() {
       return {
             UiSound: UiSound,
             music: ['shopSelect'],
+            retire: false,
       }
   },
   computed: {
         ...mapState('shopkeepData', {
             variants: state => state.shopChoice,
         }),
-        
+        ...mapState('playerData', {
+            mettleImg: state => state.info.mettleImg,
+        }),
+        ...mapState('gameData', {
+            tracker: state =>state.tracker,
+        }),
+        ...mapGetters('authData', {
+            user: 'user',
+        }),
     },
   methods: {
-    setPlayer(passedPlayer) {
-        console.log(`You're playing as the ${passedPlayer.name}: logged from CharSelect.vue`);
-        this.$store.commit('playerData/mutate', {property: 'info', with: passedPlayer});
-        this.$store.commit('gameData/mutate', {property: 'phase', with: 'DungeonPhase'});
-    },
     toShopkeep(chosenShopkeep) {
         this.$store.commit('shopkeepData/newShopkeep', {shopkeep: chosenShopkeep});
         this.$store.commit('gameData/mutate', {property: 'phase', with: 'ShopPhase'});
+    },
+    retireGame() {
+        console.log(this.user.data);
+        const recUser = this.user.data.displayName.split(" ").join("").toLowerCase();
+        const recEmail = this.user.data.email;
+
+        firebase.database().ref('users/' + recUser).set({
+            username: recEmail,
+        });
+        this.$store.commit('gameData/mutate', {property: 'phase', with: 'LoseScreen'});
+    },
+    toggleRetire() {
+        this.retire = !this.retire
+    },
+    addMettle() {
+        this.$store.commit('playerData/changeStats', {operator: 'add', stat: 'mettle', value:1});
+        this.$store.commit('gameData/mutate', {property: 'phase', with: 'DungeonPhase'});
     }
   },
   created() {
@@ -75,6 +129,21 @@ export default {
 </script>
 
 <style scoped>
+
+#about {
+    font-size: 20px;
+    position: fixed;
+    right: 10px;
+    top: 10px;
+    margin: 0;
+    padding: 5px;
+    background: rgb(218, 218, 218);
+}
+#about:hover {
+    background:rgb(185, 185, 185);
+    cursor:pointer;
+}
+
 .characterSelectWrapper {
     text-align:center;
     position:relative;
@@ -85,12 +154,46 @@ export default {
     flex-direction: row;
 }
 
+.marginTop {
+    margin-top:15px;
+}
+
 .shopkeep {
     cursor:pointer;
 }
 
 .columns {
     margin:0 10px;
+}
+
+.button {
+    background:#999;
+    margin:20px 0 0 0;
+    width:200px;
+    color:white;
+    border-radius:5px;
+    text-decoration:none;
+    padding:10px;
+    font-size:25px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    text-align:center;
+}
+
+.button img {
+    width:50px;
+    margin-left:10px;
+}
+
+.red:hover {
+    cursor:pointer;
+    background:rgb(148, 106, 106);
+}
+
+.green:hover {
+    cursor:pointer;
+    background:rgb(106, 148, 120);
 }
 
 .chooseChar img {
