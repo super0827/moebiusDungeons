d25cfd12a624e77b3323dbce3e3710b460808111
Sean Yager <contact@seanyager.com>
2020-09-09 18:18:22 -0500

    config app

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store/store'
 import { mapState, mapGetters } from 'vuex'
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8sd8gfr_MtwT-8O3DtI-nGd9T7ZRaNdA",
+  authDomain: "moebiusdungeons-9b2b6.firebaseapp.com",
+  databaseURL: "https://moebiusdungeons-9b2b6.firebaseio.com",
+  projectId: "moebiusdungeons-9b2b6",
+  storageBucket: "moebiusdungeons-9b2b6.appspot.com",
+  messagingSenderId: "693173770123",
+  appId: "1:693173770123:web:18d34ac1e8825fc2dc418e"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("authData/fetchUser", user);
+});
 
 //this export is in the global space to save time and stay concise
 //nearly if not all components mounted directly to App need these
@@ -27,6 +44,6 @@ export const mixinGlobalState = {
 Vue.mixin(mixinGlobalState);
 
 new Vue({
-  render: h => h(App),
   store,
+  render: h => h(App),
 }).$mount('#app')
