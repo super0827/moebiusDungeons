86c57303a5043e62d39a72c8b2eb4d2340022802
Sean Yager <contact@seanyager.com>
2020-10-13 11:21:04 -0500

    adding new mutations and actions to leaderboard

--- src/store/modules/leaderboardData.js
+++ src/store/modules/leaderboardData.js
@@ -5,22 +5,40 @@ const state = () => ({
   monstersKilled:0,
   monstersFought: [],
   coinsSpent:0,
+  totalCoins:0,
   totalDamageDealt:0,
   totalDamageTaken:0,
   damageBlocked:0,
   itemsBought:[],
-  totalCoins:0,
 })
 
 const mutations = {
   mutate(state, payload) {
     state[payload.property] = payload.with;
+  },
+  incrementByValue(state, payload) {
+    state.payload.property += payload.value
+  },
+  addToList(state, payload) {
+    state.itemsBought.push(payload)
+  },
+}
+
+const actions = {
+  compareToHighScore({state, commit}, payload) {
+    if(state.payload.property < payload.with) {
+      commit('mutate', payload)
+    }
   }
+}
+
+const getters = {
 }
 
 export default {
     namespaced: true,
     state,
     mutations,
+    actions,
 }
         
