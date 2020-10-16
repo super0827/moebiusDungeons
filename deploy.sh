dcce218ead204c5a9ca877747085b46b0afad4c0
Sean Yager <contact@seanyager.com>
2020-10-15 21:00:25 -0500

    added deploy.sh for master branch

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd docs
+
+# if you are deploying to a custom domain
+echo 'www.moebiusdungeons.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
+# RUN sh deploy.sh to build website for GitHub
