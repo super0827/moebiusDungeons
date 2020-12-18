6433e4499621edb7b88c6c02922694e2765fd47f
Sean Yager <contact@seanyager.com>
2020-12-18 17:40:20 -0600

    created special bar component for more abstraction

---
+++ src/components/gameUI/SpecialBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="centerRow">
+        <p>METTLE</p>
+            <img :src="mettleImg" :class="{'used': mettle <= 0}"/>
+            <img :src="mettleImg" :class="{'used': mettle <= 1}"/>
+            <img :src="mettleImg" :class="{'used': mettle <= 2}"/>
+    </section>
+</template>
+
+<script>
+
+import { mapGetters, mapState } from 'vuex';
+
+    export default {
+        name: 'SpecialBars',
+        computed: {
+             ...mapState('playerData/', {
+                mettle: state => state.info.mettle,
+                mettleImg: state => state.info.mettleImg,
+             }),
+        },
+    }
+</script>
+
+<style scoped>
+.centerRow{
+    width:200px;
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+}
+p{
+    font-size:20px;
+}
+img{
+    width:30px;
+}
+.used {
+    filter: grayscale(100%);
+}
+</style>
