3d13e774b6c7274c5a1f14383cad8360e27bfdde
Sean Yager <contact@seanyager.com>
2020-12-26 21:09:11 -0600

    new version #

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     </transition>
 
     <section class="version">
-      <p><a href="https://github.com/MisuseofMana/moebiusDungeons/blob/master/ChangeLog.md" target="_blank">v.1.3.3</a></p>
+      <p><a href="https://github.com/MisuseofMana/moebiusDungeons/blob/master/ChangeLog.md" target="_blank">v.1.3.4</a></p>
       <p class="smallText">Stable in Google Chrome</p>
       <p class="smallText">Buggy in Firefox</p>
     </section>
@@ -124,7 +124,7 @@ export default {
       })
   },
   mounted() {
-    // consoleHello();
+    consoleHello();
   },
 }
 </script>
