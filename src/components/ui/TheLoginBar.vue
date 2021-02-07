f76a241e400122f075df42b9dbeaa27e4d46cd98
Sean Yager <contact@seanyager.com>
2021-02-07 16:19:25 -0600

    refactored with bootstrapvue

--- src/components/ui/TheLoginBar.vue
+++ src/components/ui/TheLoginBar.vue
@@ -1,99 +1,73 @@
 <template>
-    <div class="bar">
-        <div class="loginBar" v-if="user.loggedIn == true">
-          <img class="avatar" :src="avatar" alt="">
-          <section class="flexRowBetween">
-            <p>{{user.data ? user.data.displayName : "..."}}</p>
-          </section>
-          <div class="settings" @click="toggleSettings">
-            <img src="@/assets/imgs/icons/settingsCog.png" alt="">
-          </div>
-        </div>
+  <b-col cols='12' class="d-flex flex-column justify-content-start align-items-center">
+    <!-- User Info Wrapper -->
+    <!-- v-If renders inside the component wrapper to maintain a centered on screen effect during preloading -->
+    <div class="loginBar d-flex flex-column justify-content-center text-center">
 
-          <p v-if="saveSuccessful">Settings Saved!</p>
-          <section v-if="!canSave && helpTip">
-          <p>
-            Game was saved!
-          </p>
-        </section>
-
-      <div v-else-if="user.data == null" class="loginBar">
-        <p @click="$store.commit('gameData/mutate', {property: 'phase', with:'Login'})" class="clickable" >Login</p>
-        <p>|</p>
-        <p @click="$store.commit('gameData/mutate', {property: 'phase', with:'Register'})" class="clickable">Sign Up</p>
+      <!-- Shows only if user is logged in -->
+      <div class="d-flex flex-row justify-content-around align-items-center"  v-if="user.loggedIn == true">
+        <b-button>
+          <b-icon scale="1.2" @click="toggleSettings" icon="gear-fill" aria-hidden="true"></b-icon> 
+        </b-button>
+        <img class="m-1 avatar" :src="avatar" alt="an avatar icon">
+        <p class="m-1">{{user.data ? user.data.displayName : "..."}}</p>
       </div>
 
-       <transition name="fade" mode="in-out">
-        <section v-if="acceptablePhase" class="flexStart highscore">
-          <p>Score:</p>
-          <h1>
-           <animated-number 
+      <!-- Helper text when user saves settings -->
+      <p v-if="saveSuccessful">Settings Saved!</p>
+      
+      <!-- Helper text when user saves their game -->
+      <section v-if="!canSave && helpTip">
+        <p>Game was saved!</p>
+      </section>
+
+      <div v-else-if="user.loggedIn == false" class="loginBar d-flex flex-row align-items-center justify-content-around text-center">
+        <b-button @click="$store.commit('gameData/mutate', {property: 'phase', with:'Login'})" class="clickable" >Login</b-button>
+        <b-button @click="$store.commit('gameData/mutate', {property: 'phase', with:'Register'})" class="clickable">Sign Up</b-button>
+      </div>
+
+      <transition name="fade" mode="in-out">
+        <section v-if="acceptablePhase" class="my-0 d-flex flex-column align-items-center justify-content-center border-top mt-2 mx-3">
+          <p class="m-0 mt-2">
+          Score: 
+          <animated-number 
           :value="highScore"
           :duration="1000"
           :formatValue="wholeNumber"
           />
-          </h1>
+          </p>
         </section>
-        </transition>
+      </transition>
 
-      <div v-if="settingShow && user.data != null" class="settings flexRowCenter">
-      <hr class="horRule">
+      <div v-if="settingShow && user.data != null" class="d-flex flex-column align-items-center mt-2">
         <section v-if="(canSave && acceptablePhase) && (user.data.displayName !== 'wanderer')" @click="saveGame" class="bigButton">
-          <h3>
-            Save Game
-          </h3>
+          <h3>Save Game</h3>
         </section>
 
         <section v-if="!canSave && acceptablePhase" @click="saveGame">
-          <p class="widthSet">
-            Your game is saved, play more before saving again.
-          </p>
+          <p>Your game is saved, play more before saving again.</p>
         </section>
         
         <section v-if="!acceptablePhase" @click="saveGame">
-          <p class="widthSet">
-            Your game can't be saved on this screen.
-          </p>
+          <p>Your game can't be saved on this screen.</p>
         </section>
-
-        <hr v-if="user.data.displayName !== 'wanderer'" class="horRule">
 
         <h2>Settings</h2>
-        <section class="flexRowStart">
-
-          <p>
-            Tooltips are:
-          </p>
-
-          <p v-if="toolTip" @click="toggleToolTips" class="button">ON</p>
-          <p v-else-if="!toolTip" @click="toggleToolTips" class="button">OFF</p>
-        </section>
-        <br v-if="user.data.displayName !== 'wanderer'">
-        <p v-if="!saveSuccessful && user.data.displayName !== 'wanderer'" @click="saveSettings" class="bigButton">Save Settings</p>
-        <p v-if="saveSuccessful" class="bigButton">Save Settings</p>
-
-      <hr class="horRule">
-      
-      <section class="flexRow widthSet">
-        <section>
-          <p class="clickable bigButton" @click="signOut">Sign Out</p>
+        <section class="d-flex flex-row align-items-center justify-content-between">
+          <h4 class="m-0 mr-1">Tooltips:</h4>
+          <b-button v-if="toolTip" @click="toggleToolTips">ON</b-button>
+          <b-button v-else @click="toggleToolTips">OFF</b-button>
         </section>
 
-        <!-- <section>
-          <p class="clickable bigButton" @click="mainMenu">Main Menu</p>
-        </section> -->
-      </section>
-
+        <section class="my-3"> 
+          <b-button class="my-4" v-if="!saveSuccessful && user.data.displayName !== 'wanderer'" @click="saveSettings">Save Settings</b-button>
+          <b-button class="mb-4" v-if="saveSuccessful">Save Settings</b-button>
+          <b-button class="mb-2" @click="signOut">Sign Out</b-button>
+          <b-button @click="mainMenu">Main Menu</b-button>
+        </section>
       </div>
-
-      <!-- <div class="prefBar flexColumn" v-if="preferences">
-        <input
-          :v-model="newName"
-          placeholder="New Display Name"
-        >
-        <button>Change Display Name</button>
-      </div> -->
     </div>
+  </b-col>
 </template>
 
 <script>
@@ -101,7 +75,6 @@ import { mapState, mapGetters } from 'vuex'
 import firebase from "firebase/app";
 import 'firebase/firestore'
 import AnimatedNumber from "animated-number-vue";
-
 
 export default {
 components: {
@@ -151,8 +124,8 @@ components: {
           case 'CreditsOverlay':
           case 'Login':
           case 'Register':
+          case 'TermsPage':
             return false;
-            break;
           default: return true;
         }
       }
@@ -205,108 +178,17 @@ components: {
 </script>
 
 <style scoped>
-.bar {
-  font-family: var(--paragraphs-type);
-  font-size:13px;
-  z-index:999999;
-  min-width:200px;
-  color:white;
-  left:0px;
-  background:rgb(54, 54, 54);
-  text-transform:uppercase;
-}
-
 .loginBar {
-  display:flex;
-  align-items:center;
-  justify-content:space-evenly;
-  padding:5px 5px;
-  min-width:200px;
-}
-
-.loginBar img{
-  width:40px;
-  height:40px;
-}
-
-.loginBar .clickable:hover {
-  cursor:pointer;
-  color:gold;
-}
-
-.settings img {
-  width:30px;
-  height:30px;
-  display:block;
-  margin:0 5px;
-  cursor:pointer;
-}
-
-.flexRowCenter {
-  display:flex;
-  align-items: center;
-  justify-content:center;
-  flex-direction: column;
-}
-
-.flexRowBetween {
-  display:flex;
-  flex-direction: row;
-  align-items: space-between;
-  justify-content: space-between;
-}
-
-.flexRowBetween p {
-  margin:0 3px;
-}
-
-.flexRowStart {
-  display:flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-}
-
-.settings {
-  padding:10px;
+  z-index:999;
+  padding:5px;
+  color:white;
+  background:rgb(54, 54, 54);
+  width:200px;
 }
 
 .avatar {
+  width:40px;
+  height:40px;
   margin:0 5px;
-}
-
-.button, .bigButton {
-  background:#777;
-  padding:2px 5px;
-  margin:0 0 0 10px;
-  cursor:pointer;
-}
-
-.button:hover, .bigButton:hover {
-  background:#ccc;
-  color:black;
-}
-
-.horRule {
-  width:100%;
-}
-
-.bigButton {
-  margin:5px;
-  padding:5px;
-}
-
-.widthSet {
-  max-width:160px;
-}
-
-.highscore p, .highscore h1 {
-  font-size:15px;
-  line-height:20px;
-  margin:0 0 10px 0;
-}
-
-.highscore p {
-  margin-right:10px;
 }
 </style>
