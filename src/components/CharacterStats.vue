802ff173aa6a7fa4b36d71962543cdb9e6bb48eb
Sean Yager <contact@seanyager.com>
2021-02-09 21:13:03 -0600

    refactored with bootstrapvue

--- src/components/CharacterStats.vue
+++ src/components/CharacterStats.vue
@@ -1,112 +1,91 @@
 <template>
-    <div>
-        <section class="stats statColumn" v-if="who === 'player'">
-
-          <ToolTip
-            subtitle="Your Health (HP)"
-            :descriptions="[`Value representing your total Health. If this reaches zero it's Game Over!`]"
-          >
-        <section class="flexRowStats">
-          <img src="@/assets/imgs/icons/healthIcon.png">
-          <p>
-          <animated-number 
-          :value="health"
-          :duration="1000"
-          :formatValue="wholeNumber"
-          />
-          </p>
-        </section>
-        </ToolTip>
-        
+  <b-row class="text-white mt-1">
+    <b-col class="mb-5 mt-3" cols="12">
         <ToolTip
-            subtitle="Your Attack (ATK)"
-            :descriptions="[`Value representing your maximum possible attack.`]"
-          >
-        <section class="flexRowStats">
-          <img :src="attackImage">
-          <p>
-          <animated-number 
-          :value="attack"
-          :duration="1000"
-          :formatValue="wholeNumber"
-          />
-          </p>
-        </section>
-        </ToolTip>
-
-        <ToolTip
-            subtitle="Your Armor (ARM)"
-            :descriptions="[`Value representing your armor, this value reduces attacks from physical type monsters.`]"
-          >
-        <section class="flexRowStats">
-          <img src="@/assets/imgs/icons/armorIcon.png">
-          <p>
-            <animated-number 
-          :value="armor"
-          :duration="1000"
-          :formatValue="wholeNumber"
-          />
-          </p>
-        </section>
-        </ToolTip>
-
-      </section>  
-
-
-      <!-- MONSTER BAR -->
-      <section class="stats statColumn" v-if="who === 'monster'">
-
-        <ToolTip
-        subtitle="Monster's HP"
-        :descriptions="[`Attacking the monster will reduce this number, when it reaches zero, you beat the monster!`]"
+          subtitle="Health (HP)"
+          :descriptions="[`Value representing total Health.`]"
         >
-        <section class="flexRowStats reverseSides">
-          <img src="@/assets/imgs/icons/healthIcon.png">
-          <p>
-          <animated-number 
-          :value="health"
-          :duration="1000"
-          :formatValue="wholeNumber"
-          />
-          </p>
-        </section>
+          <section class="d-flex flex-column justify-content-center">
+            <img class="position-absolute" src="@/assets/imgs/icons/healthIcon.png">
+            <h1 class="m-0 leftPos position-relative">
+              <AnimatedNumber 
+                :value="health"
+                :duration="1000"
+                :formatValue="wholeNumber"
+              />
+            </h1>
+          </section>
         </ToolTip>
-        
-        
-        <ToolTip
-            subtitle="Monster's Armor (ARM)"
-            :descriptions="[`Value representing the Monster's armor, this value reduces physical damage dealt to the Monster.`]"
-          >
-        <section class="flexRowStats reverseSides">
-          <img src="@/assets/imgs/icons/armorIcon.png">
-          <p>
-            <animated-number 
-          :value="armor"
-          :duration="1000"
-          :formatValue="wholeNumber"
-          />
-          </p>
-        </section>
-        </ToolTip>
-        
-        <ToolTip
-            subtitle="Monster's Armor (ARM)"
-            :descriptions="[`Value representing the Monster's armor, this value reduces physical damage dealt to the Monster.`]"
-          >
-        <section class="flexRowStats reverseSides">
-          <img :src="attackImage">
-          <p>
-          <animated-number 
-          :value="attack"
-          :duration="1000"
-          :formatValue="wholeNumber"
-          />
-          </p>
-        </section>
-        </ToolTip>
+    </b-col>
 
-      </section>  
-    </div>
+    <b-col cols="12" class="mb-5" :order="order[0]">
+      <ToolTip
+        subtitle="Attack (ATK)"
+        :descriptions="[`Value representing maximum possible physical attack.`]"
+      >
+        <section class="d-flex flex-column justify-content-center" v-if="attackType === 'physical'">
+          <img class="position-absolute" src="@/assets/imgs/icons/physicalIcon.png">
+          <h1 class="m-0 leftPos position-relative">
+            <AnimatedNumber 
+              :value="attack"
+              :duration="1000"
+              :formatValue="wholeNumber"
+            />
+          </h1>
+        </section>
+      </ToolTip>
+
+      <ToolTip
+        subtitle="Magic Attack (MAG)"
+        :descriptions="[`Value representing maximum possible magical attack.`]"
+      >
+        <section  class="d-flex flex-column justify-content-center" v-if="attackType === 'magical'">
+          <img class="position-absolute" src="@/assets/imgs/icons/physicalIcon.png">
+          <h1 class="m-0 leftPos position-relative">
+            <AnimatedNumber 
+              :value="attack"
+              :duration="1000"
+              :formatValue="wholeNumber"
+            />
+          </h1>
+        </section>
+      </ToolTip>
+    </b-col>
+
+    <b-col cols="12" class="mb-5" :order="order[1]">
+      <ToolTip
+        subtitle="Armor (ARM)"
+        :descriptions="[`Value representing armor, this value reduces physical damage taken.`]"
+      >
+        <section class="d-flex flex-column justify-content-center" v-if="armorType === 'physical'">
+          <img class="position-absolute" src="@/assets/imgs/icons/armorIcon.png">
+          <h1 class="m-0 leftPos position-relative">
+            <AnimatedNumber 
+              :value="armor"
+              :duration="1000"
+              :formatValue="wholeNumber"
+            />
+          </h1>
+        </section>
+      </ToolTip>
+
+      <ToolTip
+        subtitle="Ward (WRD)"
+        :descriptions="[`Value representing warding, this value reduces magical damage taken.`]"
+      >
+        <section class="d-flex flex-column justify-content-center" v-if="armorType === 'magical'">
+          <img class="position-absolute" src="@/assets/imgs/icons/wardIcon.png">
+          <h1 class="m-0 leftPos position-relative">
+            <AnimatedNumber 
+              :value="armor"
+              :duration="1000"
+              :formatValue="wholeNumber"
+            />
+          </h1>
+        </section>
+      </ToolTip>
+    </b-col>
+  </b-row> 
 </template>
 
 <script>
@@ -119,27 +98,28 @@ export default {
         AnimatedNumber,
         ToolTip
     },
-    props: [
-      'who',
-      'health',
-      'armor',
-      'attack',
-      'attackImage',
-    ],
+    props: ['health', 'attack', 'armor', 'attackType', 'armorType', 'order'],
 }
 </script>
 
 <style scoped>
-.flexRowStats {
-    display:flex;
-    justify-content: space-evenly;
-    align-items: center;
-}
-.reverseSides {
-  flex-direction:row-reverse;
+img {
+  width:90px;
+  height:90px;
 }
 
-p { 
-  font-size:25px;
+.leftPos {
+  text-align:center;
+  left:27px;
+  top:9px;
+}
+
+h1 {
+  height:35px;
+  width:35px;
+  font-size:20px;
+  text-align:center;
+  display:block;
+  text-shadow: 1px 1px 5px black;
 }
 </style>
