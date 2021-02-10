a509e6b075af48ffef27ff2cd3f95ebd486d1d71
Sean Yager <contact@seanyager.com>
2021-02-09 21:50:26 -0600

    refactored with bootstrapvue for better viewing on desktop

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,33 +1,67 @@
 <template>
-<section class="wrapper">
-    <h1>LEADERBOARDS</h1>
-    <p>Congragulations to the most powerful kingloyal in Amara.</p>
-    <p>Leaderboards were reset for V.1.3.2 at 12/22/2020 6pm EST</p>
-    <hr>
-    <section v-if="leaderboards.length > 0" class="flexColumn">
-        <section v-for="(items, index) in leaderboards" :key="items + index" class="block flexRow">
-            <img class="smallIcon" :src="items.data.icon">
-            <h3>{{index + 1}}. {{items.name}} | <span class="capital">{{items.data.playedAs}}</span> | {{items.data.highScore}} Points </h3>
+
+    <b-col cols="12" class="d-flex flex-column justify-content-center aligh-items-center text-center">
+        <h1>LEADERBOARDS</h1>
+        <p>Congragtulations to the most powerful kingloyal in Amara.</p>
+
+        <transition name="fade" mode="out-in">
+            <section class="mb-3">
+                <section v-if="leaderboards.length > 0">
+                    <b-card-group deck class="mb-3">
+                        <b-card
+                            v-for="(items, index) in leaderboardTop"
+                            :key="items + index"
+                        >
+                        <b-card-text>
+                            <img class="mb-1" :src="items.data.icon" alt="avatar icon from dicebear api">
+                            <h4 class="mb-1">{{index + 1 + '. ' + items.name.toUpperCase()}}</h4>
+                            <p class="mb-1">{{items.data.playedAs.toUpperCase()}}</p>
+                            <h1 class="smallFont">{{items.data.highScore}}</h1>
+                        </b-card-text>
+                        </b-card>
+                    </b-card-group>
+                    <b-card-group deck class="mb-3">
+                        <b-card
+                            v-for="(items, index) in leaderboardMid"
+                            :key="items + index"
+                        >
+                        <b-card-text>
+                            <img class="mb-1" :src="items.data.icon" alt="avatar icon from dicebear api">
+                            <h4 class="mb-1">{{index + 1 + '. ' + items.name.toUpperCase()}}</h4>
+                            <p class="mb-1">{{items.data.playedAs.toUpperCase()}}</p>
+                            <h1 class="smallFont">{{items.data.highScore}}</h1>
+                        </b-card-text>
+                        </b-card>
+                    </b-card-group>
+                    <b-card-group deck>
+                        <b-card
+                            v-for="(items, index) in leaderboardLow"
+                            :key="items + index"
+                        >
+                        <b-card-text>
+                            <img class="mb-1" :src="items.data.icon" alt="avatar icon from dicebear api">
+                            <h4 class="mb-1">{{index + 1 + '. ' + items.name.toUpperCase()}}</h4>
+                            <p class="mb-1">{{items.data.playedAs.toUpperCase()}}</p>
+                            <h1 class="smallFont">{{items.data.highScore}}</h1>
+                        </b-card-text>
+                        </b-card>
+                    </b-card-group>
+                </section>
+
+                <section v-else-if="leaderboards.length <= 0">
+                    <p>Leaderboards are empty, carve your own path and make Kingloyal history!</p>
+                </section>
+            </section>
+        </transition>
+    
+        <section>
+            <b-button size="lg" @click="home()"><h4>Main Menu</h4></b-button>
         </section>
-    </section>
-
-    <section v-else>
-        <p>Leaderboards are empty, carve your own path and make Kingloyal history!</p>
-    </section>
-
-    <br>
-
-    <section class="playAgain">    
-            <p @click="home()">Back Home</p>
-        </section>
-</section>
+</b-col>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
-import LeaderBoard from '@/store/modules/leaderboardData'
-import PlayerData from '@/store/modules/playerData'
-import BackgroundMusic from '@/plugins/BackgroundMusic'
+import { mapState } from 'vuex'
 
 export default {
   name: 'LeaderBoard',
@@ -56,6 +90,15 @@ export default {
     ...mapState('gameData', {
         leaderboards: state => state.leaderboard
     }),
+    leaderboardTop: function() {
+        return this.leaderboards.slice(0,3)
+    },
+    leaderboardMid: function() {
+        return this.leaderboards.slice(3,6)
+    },
+    leaderboardLow: function() {
+        return this.leaderboards.slice(6,9)
+    }
   },
   created() {
       this.$store.commit('gameData/clearLeaderboard')
@@ -65,77 +108,11 @@ export default {
 </script>
 
 <style scoped>
-.smallIcon {
+.card img{
     width:40px;
-    height:40px;
 }
 
-.buffer {
-    margin:5px;
-}
-
-.wrapper {
-    text-align:center;
-}
-
-.block {
-    margin:10px;
-    text-align:center;
-}
-
-.block section {
-    border:solid 2px black;
-    padding:5px;
-    margin:5px;
-}
-
-.capital {
-    text-transform:capitalize;
-}
-
-img {
-    margin-right:5px;
-}
-
-h3 {
-    line-height:40px;
-    font-size:20px;
-    margin:0;
-    padding:0;
-}
-
-h1 {
-    margin:0;
-}
-
-#gameStats {
-    border:solid 2px black;
-    padding:10px;
-    margin:10px 0px;
-}
-
-#finalStats h2 {
-    margin:0;
-    font-size:18px;
-    color:black;
-}
-
-.playAgain p {
-    background:black;
-    color:white !important;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size:20px;
-    padding:20px;
-    margin:0 auto;
-    text-align:center;
-    width:60%;
-    border:none;
-    transition:background-color 1s ease-in-out;
-}
-
-.playAgain p:hover {
-    background:rgb(102, 0, 0);
-    cursor:pointer;
-    transition:background-color 1s ease-in-out;
+.smallFont {
+    font-size:1.5vw;
 }
 </style>
