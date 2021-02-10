c1a2945a70764875ca3693a63abc2040e9f55a8e
Sean Yager <contact@seanyager.com>
2021-02-09 21:50:02 -0600

    created inventory items component

---
+++ src/components/InventoryItems.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="mt-2">
+    <b-overlay
+        id="overlay"
+        no-center
+        :show="item"
+        bg-color="#eee"
+        opacity="0.2"
+        blur="5px"
+    >
+        <template #overlay>
+            <b-row>
+                <b-col cols="12">
+                    <div>
+                        <!-- <img v-if="item" src="" alt=""> -->
+                        <h3 else>NO ITEM</h3>
+                    </div>
+                    <b-button>USE</b-button>
+                    <b-button>DISCARD</b-button>
+                </b-col>
+            </b-row>
+        </template>
+        <b-row class="blankHeight d-flex flex-row justify-content-center" no-gutters>
+            <b-col class="border m-1" cols="3"></b-col>
+            <b-col class="border m-1" cols="3"></b-col>
+            <b-col class="border m-1" cols="3"></b-col>
+        </b-row>
+        <b-row class="blankHeight d-flex flex-row justify-content-center" no-gutters>
+            <b-col class="border m-1" cols="3"></b-col>
+            <b-col class="border m-1" cols="3"></b-col>
+            <b-col class="border m-1" cols="3"></b-col>
+
+        </b-row>
+        <b-row class="blankHeight d-flex flex-row justify-content-center" no-gutters>
+            <b-col class="border m-1" cols="3"></b-col>
+            <b-col class="border m-1" cols="3"></b-col>
+            <b-col class="border m-1" cols="3"></b-col>
+        </b-row>
+    </b-overlay>
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                item: false
+            }
+        },
+    }
+</script>
+
+<style scoped>
+.blankHeight {
+    height:60px;
+}
+</style>
