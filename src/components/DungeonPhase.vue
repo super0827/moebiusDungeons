6e58a2b09fc1d51f169d80d3003e9a98a24cf9ff
Sean Yager <contact@seanyager.com>
2021-02-09 21:49:47 -0600

    refactored with bootstrapvue, added removed logic from charactertoken.vue

--- src/components/DungeonPhase.vue
+++ src/components/DungeonPhase.vue
@@ -1,146 +1,225 @@
 <template>
 <section class="dungeonPhaseWrapper" key="dungeonPhaseWrapper">
-    <section class="flexRow animated"
-    :class="{'zoomInDown' :isEntering, 'zoomOutUp' : !isEntering}">
-        <img key="dungeonSigil" id="dungeonSigil" class="iconImageSize" src="@/assets/imgs/icons/monsterSigilIcon.png" alt="">
-        <h1 class="textCenter phaseName">DUNGEON</h1>
-    </section>
+    <b-row 
+        align-h="center"
+        class="mb-3 animated"
+        :class="{'zoomInDown' :isEntering, 'zoomOutUp' : !isEntering}"
+    >
+        <b-col cols="12" class="text-center">
+            <section>
+                <img key="dungeonSigil" id="dungeonSigil" src="@/assets/imgs/icons/monsterSigilIcon.png" alt="">
+                <h1>DUNGEON PHASE</h1>
+            </section>
+        </b-col>
+    </b-row>
 
-    <!-- Battle Helper Button -->
-    <transition name="fade" mode="out-in">
-        <h1 @click="toggleHelp()" @mouseenter="UiSounds.chit.play()" id="dungeonHelp">DUNGEON HELP</h1>
-    </transition>
+    <b-row no-gutters>
+        <b-col cols="1">
+            <TurnLog
+                class="animated"
+                :class="{'zoomInLeft' : isEntering, 
+                'zoomOutLeft' : !isEntering
+                }"
+                :thisLog="playerLog"
+            />
+        </b-col>
+        <b-col cols="3">
+            <!-- Player Portrait and Stats -->
+            <CharacterToken
+            class="animated"
+            :class="{'zoomInLeft' : isEntering, 'zoomOutLeft' : !isEntering}"
+            :who="'player'"
+            :enemy="playerEnemy"
+            :name="playerName"
+            :portrait="playerPortrait"
+            :health="playerHealth"
+            :armor="playerArmor"
+            :attack="playerAttackMax"
+            :attackType="playerAttackType"
+            :attackTypeImg="playerAttackTypeImage"
+            :coins="playerCoins"
+
+            :warning="playerWarning"
+
+            :isHurt="playerisHurt"
+            :isBlocking="playerisBlocking"
+            :isAttacking="playerisAttacking"
+            :isDead="playerisDead"
+            :statSide="playerStatSide"
+            :enemyAttackType="monsterAttackType"
+            :enemyAttackTypeImage="monsterAttackTypeImage"
+            :enemyAttackDamage="monsterAttackDamage"
+            :enemyReducedAttackDamage="monsterRealDamage"
+            :porteffect="playerportEffect"
+            :redShine="redShinePlayer"
+            :greenShine="greenShinePlayer"
+            :purpleShine="purpleShinePlayer"
+            :goldShine="goldShinePlayer"
+            :blueShine="blueShinePlayer"
+            :yellowShine="yellowShinePlayer"
+            :armorUp="armorUp"
+            />
+        </b-col>
+        <b-col cols="1">
+            <CharacterStats 
+                :health="playerHealth"
+                :attack="playerAttackMax"
+                :armor="playerArmor"
+                :attack-type="playerAttackType"
+                :armor-type="playerArmorType"
+                :order="[1,2]"
+            />
+        </b-col>
+        <b-col cols="2">
+            <StatBar/>
+        </b-col>
+        <b-col cols="1">
+            <CharacterStats 
+                :health="monsterHealth"
+                :armor="monsterArmor"
+                :attack="monsterAttackMax"
+                :attack-type="monsterAttackType"
+                :armor-type="monsterArmorType"
+                :order="[2,1]"
+            />
+        </b-col>
+        <b-col cols="3">
+            <CharacterToken
+                key="monsterPortrait"
+                class="animated"
+                :class="{'zoomInRight' : isEntering, 'zoomOutRight' : !isEntering}"
+                :who="'monster'"
+                :name="monsterRank + ' ' + monsterName"
+                :enemy="monsterEnemy"
+                :portrait="monsterPortrait"
+                :health="monsterHealth"
+                :armor="monsterArmor"
+                :attack="monsterAttackMax"
+                :attackType="monsterAttackType"
+                :attackTypeImg="monsterAttackTypeImage"
+                :coins="monsterCoins"
+                :isHurt="monsterisHurt"
+
+                :warning="monsterWarning"
+
+                :isBlocking="monsterisBlocking"
+                :monsterisAttacking="monsterisAttacking"
+                :isOneShot="monsterisOneShot"
+                :isDead="monsterisDead"
+                :statSide="monsterStatSide"
+                :enemyAttackType="playerAttackType"
+                :enemyAttackTypeImage="playerAttackTypeImage"
+                :enemyAttackDamage="playerAttackDamage"
+                :enemyReducedAttackDamage="playerRealDamage"
+                :porteffect="monsterportEffect"
+                :redShine="redShineMonster"
+                :greenShine="greenShineMonster"
+                :purpleShine="purpleShineMonster"
+
+                :specialDamage="specialDamage"
+                :specialDamageAnimation="specialDamageAnimation"
+
+                :monsterRank="monsterRank"
+            />
+        </b-col>
+        <b-col cols="1">
+            <TurnLog
+                class="animated"
+                :class="{'zoomInLeft' : isEntering, 
+                'zoomOutLeft' : !isEntering
+                }"
+                :thisLog="monsterLog"
+            />
+        </b-col>
+    </b-row>
+
+    <b-row align-v="center" align-h="center" no-gutters>
+        <b-col cols="3">
+            <ToolTip
+                subtitle="Your coins"
+                :descriptions="[`Value represents your total coins, which are used to buy items during the shop phase.`]"
+            > 
+                <section class="coinWrapper">
+                    <img src="@/assets/imgs/icons/coinIcon.png" alt="">
+                    <h1 class="coinValue">
+                    <AnimatedNumber 
+                        :value="playerCoins"
+                        :duration="1000"
+                        :formatValue="wholeNumber"
+                    />
+                    </h1>
+                </section>
+            </ToolTip>
+
+            <InventoryItems/>
+        </b-col>
+        <b-col cols="4">
+            <!-- Dungeon Controls -->
+            <BattleControls
+                class="animated"
+                :class="{'zoomInUp' : isEntering, 'zoomOutDown' : !isEntering}"
+                key="battleControls"
+            />
+        </b-col>
+        <b-col cols="3">
+            <ToolTip
+                subtitle="Monster's Coins"
+                :descriptions="[`If you defeat the monster you'll win their coins.`]"
+            >
+                <section class="coinWrapper">
+                    <img src="@/assets/imgs/icons/coinIcon.png" alt="">
+                    <h1 class="coinValue">
+                    <AnimatedNumber 
+                        :value="monsterCoins"
+                        :duration="1000"
+                        :formatValue="wholeNumber"
+                    />
+                    </h1>
+                </section>
+            </ToolTip>
+
+            <InventoryItems/>
+        </b-col>
+    </b-row>
     
-    <section class="flexRow">
-        
-        <turn-log
-        class="animated"
-        :class="{'zoomInLeft' : isEntering, 
-        'zoomOutLeft' : !isEntering
-        }"
-        :thisLog="playerLog"
-        />
-
-        <!-- Player Portrait and Stats -->
-        <character-token
-        class="animated"
-        :class="{'zoomInLeft' : isEntering, 'zoomOutLeft' : !isEntering}"
-        :who="'player'"
-        :enemy="playerEnemy"
-        :name="playerName"
-        :portrait="playerPortrait"
-        :health="playerHealth"
-        :armor="playerArmor"
-        :attack="playerAttackMax"
-        :attackType="playerAttackType"
-        :attackTypeImg="playerAttackTypeImage"
-        :coins="playerCoins"
-
-        :warning="playerWarning"
-
-        :isHurt="playerisHurt"
-        :isBlocking="playerisBlocking"
-        :isAttacking="playerisAttacking"
-        :isDead="playerisDead"
-        :statSide="playerStatSide"
-        :enemyAttackType="monsterAttackType"
-        :enemyAttackTypeImage="monsterAttackTypeImage"
-        :enemyAttackDamage="monsterAttackDamage"
-        :enemyReducedAttackDamage="monsterRealDamage"
-        :porteffect="playerportEffect"
-        :redShine="redShinePlayer"
-        :greenShine="greenShinePlayer"
-        :purpleShine="purpleShinePlayer"
-        :goldShine="goldShinePlayer"
-        :blueShine="blueShinePlayer"
-        :yellowShine="yellowShinePlayer"
-        :armorUp="armorUp"
-        />
-
-        <!-- Dungeon Controls -->
-        <battle-controls
-        class="animated"
-        :class="{'zoomInUp' : isEntering, 'zoomOutDown' : !isEntering}"
-        key="battleControls"/>
-
-        <!-- Monster Portrait -->
-        <character-token
-        key="monsterPortrait"
-        class="animated"
-        :class="{'zoomInRight' : isEntering, 'zoomOutRight' : !isEntering}"
-        :who="'monster'"
-        :name="monsterRank + ' ' + monsterName"
-        :enemy="monsterEnemy"
-        :portrait="monsterPortrait"
-        :health="monsterHealth"
-        :armor="monsterArmor"
-        :attack="monsterAttackMax"
-        :attackType="monsterAttackType"
-        :attackTypeImg="monsterAttackTypeImage"
-        :coins="monsterCoins"
-        :isHurt="monsterisHurt"
-
-        :warning="monsterWarning"
-
-        :isBlocking="monsterisBlocking"
-        :monsterisAttacking="monsterisAttacking"
-        :isOneShot="monsterisOneShot"
-        :isDead="monsterisDead"
-        :statSide="monsterStatSide"
-        :enemyAttackType="playerAttackType"
-        :enemyAttackTypeImage="playerAttackTypeImage"
-        :enemyAttackDamage="playerAttackDamage"
-        :enemyReducedAttackDamage="playerRealDamage"
-        :porteffect="monsterportEffect"
-        :redShine="redShineMonster"
-        :greenShine="greenShineMonster"
-        :purpleShine="purpleShineMonster"
-
-        :specialDamage="specialDamage"
-        :specialDamageAnimation="specialDamageAnimation"
-
-        :monsterRank="monsterRank"
-        />
-
-        <turn-log
-        class="animated"
-        :class="{'zoomInRight' : isEntering,
-        'zoomOutRight' : !isEntering}"
-        :thisLog="monsterLog"
-        />
-    </section>
-    
-    <!--  Battle Helpers -->
-    <transition name="fade" mode="out-in">
-        <BattleHelp key="battleHelper" @close="toggleHelp()" v-if="helper"/>
-    </transition>
 </section>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex';
 
+import AnimatedNumber from "animated-number-vue";
+
 import BattleControls from '@/components/BattleControls.vue';
 import CharacterToken from "@/components/CharacterToken.vue";
-import BattleHelp from '@/components/BattleHelp.vue';
 import TurnLog from '@/components/TurnLog.vue';
+
+
+import StatBar from '@/components/gameUI/StatBar'
+import CharacterStats from '@/components/CharacterStats'
+import ToolTip from '@/components/ui/ToolTip'
+
+import InventoryItems from '@/components/InventoryItems'
 
 import UiSounds from '@/plugins/UiSounds.js'
 import MonsterSounds from '@/plugins/MonsterSounds.js'
 
-import helperToggles from '@/components/mixins/helperToggles';
 import gameAnimations from '@/components/mixins/gameAnimations';
 import gameMusic from '@/components/mixins/gameMusic';
 
 
 export default {
     name: 'DungeonPhase',
-    mixins: [helperToggles, gameAnimations, gameMusic],
+    mixins: [gameAnimations, gameMusic],
     components: {
-        BattleHelp,
+        ToolTip,
+        StatBar,
+        CharacterStats,
+        InventoryItems,
         CharacterToken,
         BattleControls,
         TurnLog,
+        AnimatedNumber
     },
     data() {
         return {
@@ -148,6 +227,11 @@ export default {
             UiSounds : UiSounds,
             MonsterSounds : MonsterSounds
         }
+    },
+    methods: {
+        wholeNumber(value) {
+            return `${Math.ceil(value)}`;
+        },
     },
     computed: {
         ...mapState('playerData', {
@@ -157,6 +241,7 @@ export default {
             playerPortrait: state => state.info.portrait,
             playerAttackType: state => state.info.attackType,
             playerAttackTypeImage: state => state.info.attackTypeImage,
+            playerArmorType: state => state.info.armorType,
             playerCoins: state => state.info.coins,
             playerStatSide: state => state.statSide,
             playerAttackDamage: state => state.thisDamage,
@@ -194,6 +279,7 @@ export default {
             monsterPortrait: state => state.info.portrait,
             monsterAttackType: state => state.info.attackType,
             monsterAttackTypeImage: state => state.info.attackTypeImage,
+            monsterArmorType: state => state.info.armorType,
             monsterCoins: state => state.info.coins,
             monsterStatSide: state => state.statSide,
             monsterAttackDamage: state => state.thisDamage,
@@ -255,15 +341,5 @@ export default {
 </script>
 
 <style  scoped>
-    .dungeonPhaseWrapper {
-        position:relative;
-    }
-    #dungeonSigil {
-        margin-right:10px;
-    }
-    battle-help {
-        position:absolute;
-        z-index:99999;
-        transition: all .4s ease-in-out;
-    }
+
 </style>
