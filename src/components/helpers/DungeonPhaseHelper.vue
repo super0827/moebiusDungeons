3cd1d09c4c9f4b6213890a99ae39b0bb82c54a57
Sean Yager <contact@seanyager.com>
2021-02-11 18:05:31 -0600

    reworked helper layout

--- src/components/helpers/DungeonPhaseHelper.vue
+++ src/components/helpers/DungeonPhaseHelper.vue
@@ -18,12 +18,11 @@
     <b-row v-if="shown === 'icons'" class="mt-1">
         <!-- COLUMN 1 -->
         <b-col cols="4" class="mt-2">
-            <h2>ICONS</h2>
-            <div class="d-flex align-items-start justify-content-start">
-                <div class="d-flex flex-column justify-content-start align-items-center">
-                <h3 class="m-0">HP</h3>
-                <img src="@/assets/imgs/icons/healthIcon.png" alt="a heart icon">
-                </div>
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/healthIcon.png" alt="a heart icon">
+                    HEALTH ICON <strong>[HP]</strong>
+                </h4>
                 <p>
                     Represents your health, it is reduced when monsters attack you back. There is no upper limit to your HP; be sure to buy health items while in Eyien. <strong>When this value reaches 0, you're dead, and it's GAME OVER</strong>.
                 </p>
@@ -31,109 +30,108 @@
 
             <hr>
 
-            <div class="d-flex align-items-start">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                <h3 class="m-0">ATK</h3>
-                <img src="@/assets/imgs/icons/physicalIcon.png" alt="a sword icon">
-                </div>
-                
-                <div class="d-flex flex-column">
-                    <p>
-                        This icon represents your maximum possible <strong>PHYSICAL</strong> attack power, it can be increased by buying <strong>ATK</strong> items in Eyien.
-                    </p>
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/physicalIcon.png" alt="a sword icon">
+                    ATTACK ICON <strong>[ATK]</strong>
+                </h4>
+        
+                <p class="mb-1">
+                    This icon represents your maximum possible <strong>PHYSICAL</strong> attack power, it can be increased by buying <strong>ATK</strong> items in Eyien.
+                </p>
 
-                    <p>
-                        All <strong>ATK</strong> damage is reduced the defenders <strong>ARM</strong> score, this is true of monsters and players attacking each other. <strong>ATK</strong> damage is <strong><em>NOT</em></strong> reduced by <strong>WRD</strong>.
-                    </p>
-                </div>
+                <p>
+                    All <strong>ATK</strong> damage is reduced the defenders <strong>ARM</strong> score, this is true of monsters and players attacking each other. <strong>ATK</strong> damage is <strong><em>NOT</em></strong> reduced by <strong>WRD</strong>.
+                </p>
             </div>
+
 
             <hr>
 
-            <div class="d-flex align-items-start">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                <h3 class="m-0">MAG</h3>
-                <img src="@/assets/imgs/icons/magicalIcon.png" alt="a flame icon">
-                </div>
-                
-                <div class="d-flex flex-column">
-                    <p>
-                        This icon represents your maximum possible magical attack power, it can be increased by buying attack items in Eyien.
-                    </p>
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/magicalIcon.png" alt="a sword icon">
+                    MAGIC ICON <strong>[MAG]</strong>
+                </h4>
+        
+                <p class="mb-1">
+                    This icon represents your maximum possible magical attack power, it can be increased by buying attack items in Eyien.
+                </p>
 
-                    <p>
-                        All <strong>MAG</strong> damage is reduced by the defenders <strong>WRD</strong> score, this is true of monsters and players attacking each other. <strong>ATK</strong> damage is <strong><em>NOT</em></strong> reduced by <strong>WRD</strong>.
-                    </p>
-                </div>
+                <p>
+                    All <strong>MAG</strong> damage is reduced by the defenders <strong>WRD</strong> score, this is true of monsters and players attacking each other. <strong>ATK</strong> damage is <strong><em>NOT</em></strong> reduced by <strong>WRD</strong>.
+                </p>
             </div>
         </b-col>
 
         <!-- COLUMN 2 -->
         <b-col cols="4">
-            <div class="d-flex align-items-start">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                <h3 class="m-0">ARM</h3>
-                <img src="@/assets/imgs/icons/armorIcon.png" alt="a shield icon">
-                </div>
-                <div class="d-flex flex-column">
-                    <p>
-                        This icon represents your <em>physical armor score</em>, it can be increased by the swordsmans special ability or through buying <strong>ATK</strong> items in Eyien.
-                    </p>
-                    <p>
-                        <strong>ARM</strong> is permenant, it will not be lowered by any attacks but will reduce the damage you take from monsters <strong>ATK</strong> score. 
-                    </p>
-                </div>
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/armorIcon.png" alt="a sword icon">
+                    ARMOR ICON <strong>[ARM]</strong>
+                </h4>
+        
+                <p class="mb-1">
+                    This icon represents your <em>physical armor score</em>, it can be increased by the swordsmans special ability or through buying <strong>ATK</strong> items in Eyien.
+                </p>
+
+                <p>
+                    <strong>ARM</strong> is permenant, it will not be lowered by any attacks but will reduce the damage you take from monsters <strong>ATK</strong> score. 
+                </p>
             </div>
 
-            <div class="d-flex align-items-start py-2">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                <h3 class="m-0">WRD</h3>
-                <img src="@/assets/imgs/icons/wardIcon.png" alt="an icon of three hexagons one above two">
-                </div>
-                
-                <div class="d-flex flex-column">
-                   <p>
-                        This icon represents your magical armor score, it can be increased by buying <strong>WRD</strong> items in Eyien.
-                    </p>
-                    <p>
-                        <strong>WRD</strong> is permenant, it will not be lowered by any attacks but will reduce the damage you take from monsters <strong>MAG</strong> score. 
-                    </p>
-                </div>
-            </div>
+            <hr>
+            
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/wardIcon.png" alt="a sword icon">
+                    WARD ICON <strong>[WRD]</strong>
+                </h4>
+        
+                <p class="mb-1">
+                    This icon represents your magical armor score, it can be increased by buying <strong>WRD</strong> items in Eyien.
+                </p>
 
-            <div class="d-flex align-items-start py-2">
-                <div class="d-flex flex-column justify-content-center align-items-center">
-                <h3 class="m-0">METTLE</h3>
-                <img src="@/assets/imgs/icons/allMettleIcon.png" alt="an icon of three small crystals, one red, one blue, one orange">
-                </div>
-                
-                <div class="d-flex flex-column">
-                    <p>
-                        These icon represent your <strong>mettle</strong>.
-                    </p>    
-                    <p>
-                        <strong>Mettle</strong> is consumed by using your sepecial abilities. It is replenished by resting in Eyien or purchasing <strong>Mettle</strong> items.     
-                    </p>
-                </div>
+                <p>
+                    <strong>WRD</strong> is permenant, it will not be lowered by any attacks but will reduce the damage you take from monsters <strong>MAG</strong> score. 
+                </p>
             </div>
         </b-col>
 
         <!-- COLUMN 3 -->
         <b-col cols="4">    
-            <div class="d-flex align-items-start py-2">
-                <div class="d-flex flex-column justify-content-center align-items-center mr-2">
-                <h3 class="m-0">COINS</h3>
-                <img src="@/assets/imgs/icons/coinIcon.png" alt="a golden coin icon">
-                </div>
-                
-                <div class="d-flex flex-column">
-                    <p>
-                        <strong>Coins</strong> are the currency in Moebius Dungeons.
-                    </p>    
-                    <p>
-                        Use <strong>Coins</strong> to purchase items from the safety of Eyien's walls.  
-                    </p>
-                </div>
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/allMettleIcon.png" alt="a sword icon">
+                    METTLE ICONS <strong>[MTL]</strong>
+                </h4>
+        
+                <p class="mb-1">
+                    These icon represent your <strong>mettle</strong>, a resource needed to use your special abilities.
+                </p>
+
+                <p>
+                    <strong>Mettle</strong> is consumed by using your sepecial abilities. It is replenished by resting in Eyien or purchasing <strong>Mettle</strong> items.     
+                </p>
+            </div>
+            
+            <hr>
+
+            <div>
+                <h4 class="m-0">
+                    <img src="@/assets/imgs/icons/coinIcon.png" alt="a sword icon">
+                    COIN ICON <strong>[COIN]</strong>
+                </h4>
+        
+                <p class="mb-1">
+                    <strong>Coins</strong> are the currency in Moebius Dungeons. Use <strong>Coins</strong> to purchase items from the safety of Eyien's walls.  
+
+                </p>
+
+                <p>
+                    <strong>Mettle</strong> is consumed by using your sepecial abilities. It is replenished by resting in Eyien or purchasing <strong>Mettle</strong> items.     
+                </p>
             </div>
         </b-col>
     </b-row>
@@ -143,7 +141,8 @@
     <b-row v-else-if="shown === 'moves'" class="d-flex justify-content-center">
         <b-col cols="9">
             <h2>MOVES</h2>
-            <h3>Trade Blows</h3>
+            <hr>
+            <h3>TRADE BLOWS</h3>
             <p>
                 Trade Blows is the standard attack in Moebius Dungeons.
                 This move deals a randomized amount of damage between 1 and your maximum <strong>ATK</strong> or <strong>MAG</strong> score.
@@ -193,5 +192,7 @@ export default {
 </script>
 
 <style scoped>
-
+img {
+    width:50px;
+}
 </style>
